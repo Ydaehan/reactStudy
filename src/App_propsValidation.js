e5fc5b9d4102198d3799b8aba943f7e37ad3df20
Ydaehan <daehan821@naver.com>
2023-09-25 14:42:39 +0900

    docs: react 수업 '23.09.25

--- src/App_propsValidation.js
+++ src/App_propsValidation.js
@@ -3,9 +3,15 @@ import TestProps from "./Components/exam1/TestProps"
 const App = (props)=>{
   return(
     <>
-      <TestProps first='한글' second='😊' third='テスト'/>
-      <TestProps first='dr.hong' second='홍박사' third={1000}/>
+      {/* <TestProps first='한글' second='😊' third='テスト'/>
+      <TestProps first='dr.hong' second='홍박사' third='1234'/>
+      <TestProps first='dr.hong' second='홍박사' third={1234}/> */}
+      {/* <TestProps first='dr.hong' second='홍박사' third={true}/> */}
       {/* 숫자를 props로 전달시에는 주의 필요: {110} */}
+      <TestProps/>
+      <TestProps first='dr.hong'/>
+      <TestProps first='dr.hong' second='홍박사'/>
+      <TestProps first='dr.hong' second='홍박사' third='✌️😛✌️'/>
     </>
   )
 }
--- src/Components/exam1/TestProps.js
+++ src/Components/exam1/TestProps.js
@@ -1,3 +1,4 @@
+import PropTypes  from 'prop-types';
 export default function TestProps(props){
   console.log(typeof(props.third));
   return(
@@ -5,6 +6,23 @@ export default function TestProps(props){
       <p>첫 번째 Props: {props.first}</p>
       <p>두 번째 Props: {props.second}</p>
       <p>세 번째 Props: {props.third}</p>
+      {/* <p>세 번째 Props: {props.third?"true":"false"}</p> */}
     </>
   )
+}
+
+TestProps.propTypes = {
+  // first:PropTypes.string.isRequired, // first 라는 props값은 string(문자열) 이고 필수 props이다.
+  // second:PropTypes.string.isRequired,
+  first:PropTypes.string,
+  second:PropTypes.string,
+  third:PropTypes.string,
+  // third:PropTypes.any     // any: 모든 type
+  // third:PropTypes.bool     // bool: true, false
+}
+
+TestProps.defaultProps={
+  first:'영진',
+  second:'영진 컴정',
+  third:'영진 컴정 일본IT',
 }
